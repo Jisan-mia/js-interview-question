f8e75e164d834512cab5bca319e691453d568014
Jisan-mia <jisanmia47@gmail.com>
2021-07-07 23:36:07 +0600

    convert any value into boolean value

---
+++ convertValue2boolean.js
@@ -0,0 +1,7 @@
+// convert any value to boolean
+
+// using !!(double not) in front of any value we can convert it into boolean
+console.log(!!"JavaScript"); // return true since the value is not a falsy value
+console.log(!!0); // return false since 0 is a falsy value
+console.log(!!25); // return true and we know there are only 6 falsy values(previously described)
+console.log(!!NaN); // return false since Not a Number is a falsy value
