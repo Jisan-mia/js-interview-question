bbc0bc7430c88c08c4c750c3dd976967130cd3f6
Jisan-mia <jisanmia47@gmail.com>
2021-07-08 00:56:20 +0600

    replace all occurrences of a string

---
+++ replaceStringOccurences.js
@@ -0,0 +1,6 @@
+// replace all occurrences of a string
+const quote =
+	"React is a JS framework and this framework is the most popular front-end framework right now";
+
+// replace all the occurrences of 'framework' with 'library'
+console.log(quote.replace(/framework/g, "library"));
