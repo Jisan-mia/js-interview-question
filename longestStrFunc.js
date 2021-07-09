0dee7e9018cc10a72094d8743fec3c61f3a7bbc8
Jisan-mia <jisanmia47@gmail.com>
2021-07-09 23:41:21 +0600

    longest string without loop also for multiple same lenght string

--- longestStrFunc.js
+++ longestStrFunc.js
@@ -18,24 +18,18 @@ const friendsName = ["Shawon", "Mursaline", "Porag", "Asad"];
 const longestFriend = longestStrFunc(friendsName);
 console.log(longestFriend);
 
-// same highest length
+// using for loop
+function longestString(arrOfStr) {
+	let longest = "";
+	for (let i = 0; i < arrOfStr.length; i++) {
+		if (arrOfStr[i].length > longest.length) {
+			longest = arrOfStr[i];
+		}
+	}
+	let multipleLongest = arrOfStr.filter((str) => str.length == longest.length);
 
-// function longestString(arr) {
-// 	var wholeArr = [];
-//     var tlength = 0;
-//     for(var i =0; i < plorp.length; i++){
-//       if(tlength < plorp[i].length){
-//         tlength = plorp[i].length;
-//       }
-//     }
-//     for(var j =0; j < plorp.length; j++){
-//       if(plorp[j].length == tlength){
-//          wholeArr.push(plorp[j]);
-//       }
-//     }
-//    if(wholeArr.length == 1){
-//      return wholeArr[0]
-//    }else{
-//       return wholeArr
-//   }
-// }
+	return multipleLongest.length > 1 ? multipleLongest : longest;
+}
+console.log(
+	longestString(["Shawon", "Mursaline", "jisanmiaa", "Porag", "Asad"])
+);
