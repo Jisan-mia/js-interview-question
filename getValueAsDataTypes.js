b72bd5f364c98085e5086444b25f26c8833bf59e
Jisan-mia <jisanmia47@gmail.com>
2021-07-21 01:50:02 +0600

    remove duplicate from arry using set"

--- getValueAsDataTypes.js
+++ getValueAsDataTypes.js
@@ -2,6 +2,7 @@
 // by default input element values data type is string
 // if we want to get any specific input element values as number data types
 // simply add 'valueAsNumber' at the end of that element finding
+// also by adding 'valueAsData' at the end will return date data types
 
 function getDataType() {
 	const inputNumber = document.getElementById("inputNumber").valueAsNumber;
