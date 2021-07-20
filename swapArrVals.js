65d9d5fa6d52f04ef70fc5b965837607d3d7b46e
Jisan-mia <jisanmia47@gmail.com>
2021-07-21 00:18:24 +0600

    swap variable vals

--- swapArrVals.js
+++ swapArrVals.js
@@ -6,3 +6,7 @@ let temp = arr[0];
 arr[0] = arr[4];
 arr[4] = temp;
 console.log(arr);
+
+// swap arr values in one line using array destructuring
+[arr[0], arr[4]] = [arr[4], arr[0]];
+console.log(arr);
---
+++ swapVal.js
@@ -0,0 +1,19 @@
+// swap variable values
+// suppose we have 2 variables a and b, we want to swap there value
+// for example the value of variable a and b are 2,3. the result would be a = 3, and b = 2
+
+// using temp variable
+let a = 2;
+let b = 3;
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b); // expected output: a = 3, b = 2
+
+// // using array destructuring
+// [a, b] = [b, a];
+// console.log(a, b); // expected output: a = 3, b = 2
+
+// using math in one line
+// b = a + (a = b) - b;
+// console.log(a, b); // expected output: a = 3, b = 2
