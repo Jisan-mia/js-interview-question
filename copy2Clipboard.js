fdc1daedd8e0613884dd1ea8c4916a3461914d4b
Jisan-mia <jisanmia47@gmail.com>
2021-07-21 00:52:17 +0600

    copy two clipboard tricks using JavaScript

---
+++ copy2Clipboard.js
@@ -0,0 +1,14 @@
+function copyToClipboard(text) {
+	let textArea = document.createElement("textarea");
+	textArea.value = text;
+	document.body.appendChild(textArea);
+	textArea.select();
+	document.execCommand("copy");
+	document.body.removeChild(textArea);
+	alert("copied successfully");
+}
+
+function handleCopy() {
+	const text = document.getElementById("text");
+	copyToClipboard(text.innerText);
+}
