17e0ca4262cb61ec938aaa4d53569795a342225a
Jisan-mia <jisanmia47@gmail.com>
2021-07-24 23:57:34 +0600

    tagged templete literals

---
+++ notCalledFunc.js
@@ -0,0 +1,10 @@
+// what will be the output of below code
+function myFunc() {
+	return "JavaScript";
+}
+
+const result = myFunc`hello`;
+// what we did here is:
+// we write the func name
+//  we used template literals and write hello
+console.log(result);
