dc4d4e6836b727d5629286ca1ec47f84ef157b0e
Jisan-mia <jisanmia47@gmail.com>
2021-07-30 00:08:54 +0600

    string constructor property

--- stringConstructor.js
+++ stringConstructor.js
@@ -6,7 +6,8 @@ console.log(a[a](01)); // output: 1
 // we know string is iterable
 // first a is string
 // second a is 'constructor'
-// here we are getting string constructor property the big string function(String())
+// here we are getting string constructor property and it is the big string function(String())
 // so a[a] = String
 // and we are passing 01 to String()
+// whatever we passes there it becomes string
 // like this String(01) = '1'
